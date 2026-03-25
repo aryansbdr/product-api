cb5bf47550d656dec6536c514eb1b5a817e1c26c
muhhabibie <144841901+muhhabibie@users.noreply.github.com>
2026-03-26 01:15:55 +0700

    feat: Tambah validasi data pada ProductRequest DTO

--- src/main/java/com/example/product_api/dto/ProductRequest.java
+++ src/main/java/com/example/product_api/dto/ProductRequest.java
@@ -1,10 +1,22 @@
 package com.example.product_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class ProductRequest {
+
+    @NotBlank(message = "Nama produk tidak boleh kosong")
     private String name;
+
+    @NotNull(message = "Harga tidak boleh kosong")
+    @Positive(message = "Harga harus lebih besar dari 0")
     private Double price;
+
+    @NotNull(message = "Stok tidak boleh kosong")
+    @PositiveOrZero(message = "Stok tidak boleh negatif")
     private Integer stock;
 }
